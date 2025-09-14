7dfd99c9150910fab7d4ac59a5f50f5dced9916a
AmirrezaSalimzadeh <maktabamirreza@gmail.com>
2025-09-14 23:38:34 +0330

    the admin dashboard is created and modified

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Quotes
 
-# Register your models here.
+@admin.register(Quotes)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ("content", "author", "created_at", "featured")
+    list_display_links = ("content",)
+    list_editable = ("featured",)
+    ordering = ("-created_at",)
+    list_per_page = 20
+
+    search_fields = ("content", "author")
+    list_filter = ("author", "created_at", "featured")
+    date_hierarchy = "created_at"
+
+    readonly_fields = ("created_at",)
+
+    fieldsets = (
+        ("Quote Details", {"fields": ("content", "author")}),
+        ("Metadata", {"fields": ("created_at", "featured")}),
+    )
+
+    actions = ["mark_as_featured"]
+
+    def mark_as_featured(self, request, queryset):
+        queryset.update(featured=True)
+    mark_as_featured.short_description = "Mark selected quotes as featured"
+
--- polls/models.py
+++ polls/models.py
@@ -4,4 +4,9 @@ from django.db import models
 class Quotes(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=100)
-    added_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # new field
+    featured = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True,null=True)         # new field
+
+    def __str__(self):
+        return f'"{self.content}" by {self.author}'
