95961433fb072a66fa55d525a9f3f42974747b87
AmirrezaSalimzadeh <maktabamirreza@gmail.com>
2025-09-15 00:11:24 +0330

    the base.html is added and also the main theme of the website has changed and we are know have sign up and sign in in the website with also sign in and sign up html web pages

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("", views.Home, name="home"),
     path("home/", views.Home, name="home"),
     path("about/", views.about, name="about"),
-    path("add_quote/",views.add_quote,name ="add_quote"),
+    path("add_quote/", views.add_quote, name="add_quote"),
+    path("signup/", views.signup, name="signup"),
+    path("signin/", views.signin, name="signin"),
+    path("signout/", views.signout, name="signout"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .utils import randomQuote
 from .models import Quotes
+
 # Create your views here.
 def Home(request):
-    author, quote1 = randomQuote()
-    return render(request, "Home.html", {"quote": quote1, "author": author})
+    author, quote = randomQuote()
+    return render(request, "Home.html", {"quote": quote, "author": author})
 
 def about(request):
     return render(request,"about.html")
@@ -16,3 +20,36 @@ def add_quote(request):
         Quotes.objects.create(content=content, author=author)
         return redirect("home")  # after saving
     return render(request, "add_quotes.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        if password != password2:
+            messages.error(request, "Passwords do not match.")
+            return render(request, "signup.html")
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return render(request, "signup.html")
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect("home")
+    return render(request, "signup.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return render(request, "signin.html")
+    return render(request, "signin.html")
+
+def signout(request):
+    logout(request)
+    return redirect("home")
